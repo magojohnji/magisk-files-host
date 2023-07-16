a81c989cfb9a29b9b43af79b7f8f0e066df598a6
v阿布Abu <69509738+magojohnji@users.noreply.github.com>
2023-07-16 15:13:46 +0800

    Create checkdir.sh

---
+++ checkdir.sh
@@ -0,0 +1,23 @@
+if [ ! -d "./offical" ];then
+ mkdir offical
+fi
+
+if [ ! -d "./delta" ];then
+ mkdir offical
+fi
+
+if [ ! -d "./alpha" ];then
+ mkdir offical
+fi
+
+if [ ! -d "./lite" ];then
+ mkdir offical
+fi
+
+if [ ! -d "./monet" ];then
+ mkdir offical
+fi
+
+if [ ! -d "./cygisk" ];then
+ mkdir offical
+fi
